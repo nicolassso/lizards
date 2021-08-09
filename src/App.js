80ec21267f7a9ba607085c41573029c255195038
nicolassso <nicolasleal1996@gmail.com>
2021-08-09 14:18:10 +0200

    mobile version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import {Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 
 import SignUpPage from './components/pages/sign-up-page/sign-up-page.component';
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.scss';
 // import Logo from '../../images/logo-guitar.PNG'
 
